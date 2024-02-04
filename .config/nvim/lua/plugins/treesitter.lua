1d2b6af56a97bb8c3932818697f218ed754a3f1f
Sk Arif <skarif2@gmail.com>
2024-02-05 00:28:58 +0600

    added .config and .zshrc

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "css",
+        "gitignore",
+        "graphql",
+        "http",
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+    end,
+  },
+}
